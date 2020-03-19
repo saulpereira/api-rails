6b24470926d56bd98b580c4687d94c4f3da95483
saulpereira <saul_pereira@hotmail.com>
2020-03-19 11:49:07 -0600

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+
+    def index
+        articles = Article.recent.page(params[:page]).per(params[:per_page])
+        render json: articles
+    end
+
+    def show
+    end
+end
